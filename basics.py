05098ba8f671ab6998aaf99da667d7693c33f762
rohitbathla2005 <rohitbathla1986@gmail.com>
2023-05-29 10:22:32 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,23 @@
+print("Numbers ,types and ids")
+
+z = 10
+print(type(z),id(z),z)
+a = 23.7
+print(type(a),id(a),a)
+b = 2+6j
+print(type(b),id(b),b)
+
+k = 'h'
+print(type(k),id(k),k)
+l = "hello"
+print(type(l),id(l),l)
+
+a = True
+print(type(a),id(a),a)
+a=[1,2,3]
+print(type(a),id(a),a)
+a=(1,2,3)
+print(type(a),id(a),a)
+a={1,2,3}
+print(type(a),id(a),a)
+
